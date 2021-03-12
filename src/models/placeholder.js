fba16b617efaaa12106c28851e14698bb420222a
damishra <dxm2269@rit.edu>
2021-03-12 18:11:54 -0500

    eg: added sample code

---
+++ src/models/placeholder.js
@@ -0,0 +1,15 @@
+const User = sequelize.define(
+  'User',
+  {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+  },
+  {}
+);
+
+module.exports = User;
